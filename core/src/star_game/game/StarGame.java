b76e86a3a311a55b1c6770ed42ad4dd51fd75efe
Piven Vadim <vp29101998q@gmail.com>
2021-05-22 20:46:15 +0900

    add background

--- core/src/star_game/game/StarGame.java
+++ core/src/star_game/game/StarGame.java
@@ -3,22 +3,29 @@ package star_game.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private TextureRegion backgroundTexture;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backgroundTexture = new TextureRegion(
+				new Texture("background.jpg")
+				, 0, 0, 1920, 1180
+		);
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
+		batch.draw(backgroundTexture, 0, 0);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
